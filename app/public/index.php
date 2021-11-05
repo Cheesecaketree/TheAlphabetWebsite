919d03fb330c9103245c4b8820a1fc504331b261
Cheesecaketree <holycaketree@gmail.com>
2021-11-05 11:14:00 +0100

    first commit

---
+++ app/public/index.php
@@ -0,0 +1,12 @@
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Das Alphabet</title>
+        <link rel="stylesheet" href="css/style.css">    
+    </head>
+
+<body>
+    <h1>Hallo!</h1>
+</body>
+
+</html>
