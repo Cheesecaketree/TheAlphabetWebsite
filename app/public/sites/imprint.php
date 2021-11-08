8645f14f490673de360d8d542eff9a6e14f1f000
Cheesecaketree <holycaketree@gmail.com>
2021-11-08 15:25:37 +0100

    adapted color gen from main site

--- app/public/sites/imprint.php
+++ app/public/sites/imprint.php
@@ -3,9 +3,15 @@
         <meta charset="utf-8">
         <title>Das Alphabet</title>
         <link rel="stylesheet" href="../css/style.css">    
+        <script type='text/javascript' src='../js/colors.js'></script>
     </head>
 
     <body id="cBackground">
+        <script type="text/javascript">
+            window.onload = function() {
+                main();
+            }
+        </script>
         <div class="centered">
             <div class="imprint_text">
                 <p>
@@ -21,25 +27,5 @@
     </body>
 
     <? echo file_get_contents("./footer.html") ?> 
-
-    <script type='text/javascript' src='../js/TinyColor-master/tinycolor.js'></script> <!-- https://github.com/bgrins/TinyColor -->
-    <script type='text/javascript'>
-        var background = document.getElementById('cBackground'); 
-        var colored = document.getElementsByClassName('colored');
-
-        var colors = ["#77dd77", "#89cff0", "#ff6961", "#ff694f", "#ca9bf7", "#ffb7ce", "#fdfd96", "#cb99c9", "#bee7a5", "#befd73", "#bdb0d0", "#b2fba5"];
-        var rand_index = Math.floor(Math.random() * colors.length);
-
-        var char_color = colors[rand_index];
-        var backg_color = tinycolor(char_color).spin(180).toString();
-
-        background.style.backgroundColor = backg_color;
-
-        for(var i = 0; i < colored.length; i++) {
-            colored[i].style.color = char_color;
-        }
-
-        
-    </script>
 
 </html>
