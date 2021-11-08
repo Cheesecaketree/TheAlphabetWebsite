c88b23cc69dd8c689af6bc65d545c8376279d9b3
Cheesecaketree <holycaketree@gmail.com>
2021-11-08 17:03:19 +0100

    renaming

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -23,7 +23,7 @@ function spinHue(hue, amount) {
     return nHue;
 }
 
-function main() {
+function setColors() {
     const hue = randomHue();
     const sat = randomSat(99, 100);
     const light = randomLight(65, 85);
@@ -31,18 +31,26 @@ function main() {
     // making the colors
     var letterColor = getHSL(hue, sat, light);
     var backgroundColor = getHSL(spinHue(hue, 180), sat, light);
-    //console.log(letterColor);
-    //console.log(backgroundColor);
-
 
     // getting elements
     var background = document.getElementById('cBackground');
     var colored = document.getElementsByClassName('colored');
-
 
     background.style.backgroundColor = backgroundColor;
 
     for(var i = 0; i < colored.length; i++) {
         colored[i].style.color = letterColor;
     }
+}
+
+function setLetter() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const element = document.getElementById("let");
+
+    element.innerHTML = alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+function startFunction() {
+    this.setLetter();
+    this.setColors();
 }
